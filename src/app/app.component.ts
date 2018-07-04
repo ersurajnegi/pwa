88ce79e6251500d882f49ec61cdd4756b7686ff6
Suraj Negi <s.negi@agilecockpit.com>
2018-07-04 16:00:39 +0530

    nfghjf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'Poker App By Cockpit';
 
-  constructor(private updates: SwUpdate) {}
+  navigator: any;
+  @HostListener('window:offline', ['$event'])
+  offline(event) {
+    this.snackBar.open('You are Offline', 'Close', { duration: 2000 });
+  }
+
+  @HostListener('window:online', ['$event'])
+  online(event) {
+    this.snackBar.open('You are Online', 'Close', { duration: 2000 });
+  }
+
+  constructor(private updates: SwUpdate, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     console.log('checking Update');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,15 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NoopAnimationsModule,
     MatExpansionModule,
-    MatDatepickerModule,
-    MatInputModule,
-    MatNativeDateModule,
+    MatSnackBarModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
